fbd10aa4ab9421c14e1c3dc98e22b3cd04ff5f2a
pramitjagate <126240401+pramitjagate@users.noreply.github.com>
2023-02-25 09:08:12 +0530

    addes successfully

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Counter from './countercomponent/Counter'
 import Faker from './Faker'
 import Toast from './Toast'
 import Hooks from './Hooks';
+import Counter1 from './Counter1'
 
 export default class App extends Component {
   render() {
     return (
       <>
-      Counterusinghooks
+      <Counter1 />
       </>
     )
   }
